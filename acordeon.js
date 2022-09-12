477e754889f7e66c1b7b3433a0189af509c875a6
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-09-12 14:44:20 -0500

    accordion ready

--- acordeon.js
+++ acordeon.js
@@ -1,14 +1,10 @@
 window.addEventListener('load',()=>{
-    const acordeon = document.querySelector('.acordeon');
     const despliegue = document.querySelectorAll('.bloque');
     const titulo = document.querySelectorAll('.titulo')
     
     titulo.forEach((item,idx)=>{
         item.addEventListener('click',()=>{
-       
-
-            despliegue[idx].classList.toggle('activo')        
-
+            despliegue[idx].classList.toggle('activo')
         })
     })
 })
